94a88b755c2e0b38a718e0ec133d46fc25d03fc8
prim0411993 <prim0411993@users.noreply.github.com>
2024-11-19 12:42:24 +0000

    deploy: cf801af1a321debdfcbf08970aac4af2c956dd75

--- src/App.test.js
+++
@@ -1,9 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('renders greeting message', () => {
-  render(<App />);
-  // Проверка правильного текста
-  const linkElement = screen.getByText(/Каличенок Сергей Николаевич/i);
-  expect(linkElement).toBeInTheDocument();
-});
